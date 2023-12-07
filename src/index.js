042609606c046b9773d3d3d237f0b3dda75c82ae
djv007 <dhananjayvaish@gmail.com>
2023-12-07 13:07:35 +0530

    Create React project via webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 // const root = document.getElementById('root');
 // const p = document.createElement('p');
 // p.innerText = "Hello from dj";
@@ -6,8 +9,8 @@
 
 const child1 = React.createElement('p' , {} , "I am a child1")
 const child2 = React.createElement('p', {}, "I am a child2")
-const div = React.createElement('div', { className: 'text' }, [child1, child2])
+// const div = React.createElement('div', { className: 'text' }, [child1, child2])
 console.log(div)
-// const div = <div className = 'text'>Hello 2 </div>
+const div = <div className = 'text'>Hello 2 </div>
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(div)
